b442e3d79e2e3d84842e7ffa30532f32351b068d
gabyfinn <gabrieluzzante@gmail.com>
2023-11-07 21:07:40 -0300

    Pusheanding

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log('Servidor funcionando');
 });
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html');
 });
 
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/views/login.html');
 });
 
-app.get('/register', (req,res)=>{
+app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
 });
